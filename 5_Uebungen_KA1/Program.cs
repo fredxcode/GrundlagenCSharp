eca1a10fe7b90173ab5b279692b1deb2c1137364
fredxcode <frederik.hamm.fh@gmail.com>
2022-08-31 14:23:35 +0200

    Lösung der Aufgabe

---
+++ 5_Uebungen_KA1/Program.cs
@@ -0,0 +1,2 @@
+﻿string sHöhe = "Höhe", sBreite = "Breite";
+float fHöhe = 32.346F, fBreite = 15.193F;Console.WriteLine("  1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20");Console.WriteLine("1             H ö h e              B  r  e  i  t  e ");
